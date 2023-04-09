f0d52eceb41044754e93cc5e0146d3a57402fbd9
babalar-press <babalarpress@gmail.com>
2023-04-09 23:04:05 +0200

    new

--- docs/js/sketch.js
+++ docs/js/sketch.js
@@ -5,7 +5,7 @@
 // RANDOM POETIC COMPUTATION
 
     var babalarpress = [
-        "BABALAR PRESS is a Qazaq experimental publishing initiative for relearning, reclaiming and reimagining our ancestral knowledge(s).", "BABALAR PRESS – бұл бабаларымыздан қалған білімді зерттеп, түсіну және (қайта) қарауға бағытталған қазақ эксперименттік зерттеу, жазу және басылым бастамасы.", "BABALAR PRESS — это казахстанская экспериментальная исследовательская, писательская и издательская инициатива для (пере)осмысления и возвращения себе знаний наших предков."
+        "BABALAR PRESS is a Qazaq experimental publishing initiative for relearning, reclaiming and reimagining our ancestral knowledge(s).", "BABALAR PRESS – бұл бабаларымыздан қалған білімді зерттеп, түсіну және (қайта) қарауға бағытталған қазақ эксперименттік басылым бастамасы.", "BABALAR PRESS — это казахстанская экспериментальная издательская инициатива для (пере)осмысления и возвращения себе знаний наших предков."
         ];
     
         randDef = babalarpress[Math.floor(Math.random() * babalarpress.length)];
